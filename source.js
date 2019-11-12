659f9ed8b202534614761e294755aebd1ae3177e
Meagan Sullivan <sullivan.meagan.e@gmail.com>
2019-11-11 18:54:36 -0700

    test select

--- source.js
+++ source.js
@@ -1,9 +1,8 @@
 const { loadRestaurants, getAvailableRestaurants } = require('./restaurants');
 
-window.onload = loadRestaurants();
-
-window.addEventListener('click', event => {
-    const list = document.getElementById("restaurantSpot");
-    const restaurants = document.createTextNode(getAvailableRestaurants(0,0));    
-    list.appendChild(restaurants);
-});
+new Vue({
+    el:'#days',
+    data: {
+        selected: ''
+    }
+})
